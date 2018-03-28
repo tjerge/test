092a1ad06b566ff5af50da4d61546b6d88e783e2
tjerge <srgdwlng@googlemail.com>
2018-03-28 23:11:46 +0100

    dfsfd

--- newtest.js
+++ newtest.js
@@ -1,8 +1,6 @@
 var http = require('http');
 
-http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  });
-  res.end('Hello World!');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
 }).listen(8080);
